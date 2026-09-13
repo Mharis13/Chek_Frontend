3f7f6912d71572400ca7baa0ce93cb7d05713fa4
Alastair7 <alexandru.ban.drmer@gmail.com>
2026-09-13 20:13:56 +0200

    Fix linting warnings

--- eslint.config.js
+++ eslint.config.js
@@ -1,14 +1,12 @@
 // https://docs.expo.dev/guides/using-eslint/
-import { eslinntPluginPrettierRecommended } from "eslint-plugin-prettier/recommended"
-import { defineConfig } from "eslint/config"
-import expoConfig from "eslint-config-expo/flat"
-
-
+import eslintPluginPrettierRecommended from "eslint-plugin-prettier/recommended";
+import { defineConfig } from "eslint/config";
+import expoConfig from "eslint-config-expo/flat.js";
 
 export default defineConfig([
   expoConfig,
-  eslinntPluginPrettierRecommended,
+  eslintPluginPrettierRecommended,
   {
     ignores: ["dist/*"],
-  }
+  },
 ]);
